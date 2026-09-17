e60eeb0d23b0aacce952f0645f5fc728843657a1
Justin Arambam <justinarambam212@gmail.com>
2026-09-17 14:28:22 +0530

    qw

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,7 @@ const paymentRoutes = require("./routes/payment");
 
 const app = express();
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 
 // ======================================================
 // MIDDLEWARE
